35d9d05f8bf0f85a9eceb6f957593c0176cbe5ad
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-03-18 11:57:05 +0700

    2.2: инициализация таблиц

--- dis2/src/main/java/ru/nsu/ccfit/beloglazov/dis/dis2/Main.java
+++ dis2/src/main/java/ru/nsu/ccfit/beloglazov/dis/dis2/Main.java
@@ -1,6 +1,7 @@
 package ru.nsu.ccfit.beloglazov.dis.dis2;
 
 import org.apache.log4j.Logger;
+import java.util.List;
 
 public class Main {
 
@@ -10,12 +11,20 @@ public class Main {
 //        new CompressedOsmParser().parse("RU-NVS.osm.bz2");
 
         try {
-            DatabaseInitializer.initialize();
+            DatabaseInitializer dt = new DatabaseInitializer();
+            log.info("Successfully connected to local database");
+            List<String> tables = dt.getTables();
+            log.info("Tables in database: " + dt.getTables());
+            if (!tables.contains("nodes")) {
+                dt.createNodesTable();
+                log.info("Created table 'nodes'");
+            }
+            if (!tables.contains("tags")) {
+                dt.createTagsTable();
+                log.info("Created table 'tags'");
+            }
         } catch (Exception e) {
             log.error(e.getMessage());
-            return;
         }
-
-        log.info("Successfully connected to local database");
     }
 }
